14cb925f8e11010b4e0a6159922868df222cb95b
htack210 <htack210@live.com>
2024-06-04 15:37:01 -0400

    Handling unexpected errors

---
+++ app/not-found.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const NotFoundPage = () => {
+  return (
+    <div>
+      <h1 className="text-center align-middle">
+        Dude! Can't find the page you're looking for!
+      </h1>
+    </div>
+  );
+};
+
+export default NotFoundPage;
---
+++ app/users/[id]/not-found.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const UserNotFoundPage = () => {
+  return (
+    <div className="text-center align-middle">
+      <h1>Dude! Cannot find this user. It's like they don't exist!</h1>
+      <p>Whooooaa!</p>
+    </div>
+  );
+};
+
+export default UserNotFoundPage;
