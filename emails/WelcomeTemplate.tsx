3aa55e6fd7b6aa52acce6cc30dc17992976110d1
htack210 <htack210@live.com>
2024-06-10 18:14:14 -0400

    Styling emails.

--- emails/WelcomeTemplate.tsx
+++ emails/WelcomeTemplate.tsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { CSSProperties } from "react";
 import {
   Html,
   Body,
   Container,
+  Tailwind,
   Text,
   Link,
   Preview,
@@ -12,14 +13,25 @@ const WelcomeTemplate = ({ name }: { name: string }) => {
   return (
     <html>
       <Preview>Welcome, {name}!</Preview>
-      <Body>
-        <Container>
-          <Text>Hello, and how are you?</Text>
-          <Link href="https://mybook.to/aio">Worst Sellers!</Link>
-        </Container>
-      </Body>
+      <Tailwind>
+        <Body className="bg-slate-50">
+          <Container>
+            <Text className="font-bold text-3xl font-sans">
+              Hello, and how are you?
+            </Text>
+            <Link href="https://mybook.to/aio">Check out my books!</Link>
+          </Container>
+        </Body>
+      </Tailwind>
     </html>
   );
+};
+
+const body: CSSProperties = {
+  background: "whitesmoke",
+};
+const heading: CSSProperties = {
+  fontSize: "32px",
 };
 
 export default WelcomeTemplate;
