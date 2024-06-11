bec39cb28ac76696aba6fcfae26eb5d4952e4915
htack210 <htack210@live.com>
2024-06-11 15:14:56 -0400

    SEO

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import ProductCard from "./components/ProductCard";
 import { getServerSession } from "next-auth";
 import { authOptions } from "./api/auth/[...nextauth]/route";
 import bryson from "@/public/images/Bryson.jpeg";
+import { Metadata } from "next";
 
 export default async function Home() {
   const session = await getServerSession(authOptions);
@@ -33,4 +34,19 @@ export default async function Home() {
       {/* <ProductCard /> */}
     </main>
   );
+}
+
+// export const metadata: Metadata = {
+//   title: "Awesomest Next Site ever!",
+//   description: "Only the best NextJS website (written by me).",
+// };
+
+export async function generateMetadata(): Promise<Metadata> {
+  const product = await fetch("");
+
+  return {
+    //Do these without quotes
+    title: "product.title",
+    description: "product.description",
+  };
 }
